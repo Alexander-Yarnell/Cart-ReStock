dcd5bf8c285bce9f754178b59c2395b64c78d9e7
Alexander Yarnell <yarnell.alexander@yahoo.com>
2021-06-08 06:12:39 -0500

    Working version of bug.

---
+++ cartDB/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9f44271db52b7f132646c7931d22e44c'),
+    },
+  },
+});
---
+++ cartDB/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '375394c4-f9e8-4d8c-b502-761bc8b8bd37'
+};
